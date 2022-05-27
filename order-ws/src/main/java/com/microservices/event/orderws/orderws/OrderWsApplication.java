8194774d4d5f74e8fe3e11b3b2f59502347b6d78
Ioana Tiriac <ioana.tiriac@gmail.com>
2022-05-27 13:12:03 +0200

    added new orders microservice

---
+++ order-ws/src/main/java/com/microservices/event/orderws/orderws/OrderWsApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.event.orderws.orderws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderWsApplication.class, args);
+	}
+
+}
---
+++ order-ws/src/test/java/com/microservices/event/orderws/orderws/OrderWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.event.orderws.orderws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
