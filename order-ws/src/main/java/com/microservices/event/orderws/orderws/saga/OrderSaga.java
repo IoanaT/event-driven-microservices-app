0d8f266efbb52d491216b364f5274991a6644581
Ioana Tiriac <ioana.tiriac@gmail.com>
2022-05-29 20:05:04 +0200

    add Saga class and core dependency to order microservice

---
+++ order-ws/src/main/java/com/microservices/event/orderws/orderws/saga/OrderSaga.java
@@ -0,0 +1,37 @@
+package com.microservices.event.orderws.orderws.saga;
+
+import com.microservices.event.core.commands.ReserveProductCommand;
+import com.microservices.event.orderws.orderws.core.events.OrderCreatedEvent;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+
+@Saga
+public class OrderSaga {
+
+    private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+        ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+                .orderId(orderCreatedEvent.getOrderId())
+                .productId(orderCreatedEvent.getProductId())
+                .quantity(orderCreatedEvent.getQuantity())
+                .userId(orderCreatedEvent.getUserId())
+                .build();
+        commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+            @Override
+            public void onResult(CommandMessage<? extends ReserveProductCommand> commandMessage,
+                                 CommandResultMessage<?> commandResultMessage) {
+                if(commandResultMessage.isExceptional()){
+                    //start a compensating transaction
+                }
+            }
+        });
+    }
+}
