c0ba8e48351ef9b95472da397868ff0bd527538f
Ioana Tiriac <ioana.tiriac@gmail.com>
2022-05-31 15:21:15 +0200

    added users microservice to project

---
+++ users-ws/src/main/java/com/microservices/event/usersws/UsersWsApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.event.usersws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersWsApplication.class, args);
+	}
+
+}
